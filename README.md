09e70c10445901c24dee10ee1cb70d596863761c
rijksly <senia9613@gmail.com>
2021-08-03 22:32:44 +0300

    receive.py and main.py were merged, README.md was changed

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Peer-to-peer python chat
 
-main.py sends text 'Hello, world' to receiver.py
+Script that can send one message between two computers.
 
 ## Dependencies
 
@@ -10,10 +10,6 @@ main.py sends text 'Hello, world' to receiver.py
 
 - main.py:
 
-      python main.py (ip of computer with other file running) (port)
+      python main.py (IP address of computer with other file running) (send port) (receive port)
 
-- receiver.py
-
-      python receiver.py (ip of computer with other file running) (port)
-
-Port must be the same.
+Port must be the same on both computers (send on first - receive on second etc.).
--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 from sys import argv
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((argv[1], int(argv[2])))
-print('connected')
+def init_send_socket(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, int(port)))
+    print('send socket connected')
 
-s.send(bytes('Hello, world', encoding = 'UTF-8'))
+    return s
 
-s.close()
+def init_accept_socket(ip, port):
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind((ip, int(port)))
+    serversocket.listen(1)
+    (clientsocket, address) = serversocket.accept()
+    print('accept socket connected')
+
+    return clientsocket
+
+read_or_write = input('Would you like to send (s) or to receive (r)?: ')
+
+if read_or_write == 's':
+    send = init_send_socket(argv[1], argv[2])
+
+    message = input('Message: ')
+    send.send(bytes(message, encoding = 'UTF-8'))
+elif read_or_write == 'r':
+    receive = init_accept_socket(argv[1], argv[3])
+
+    while True:
+        data = receive.recv(1024)
+        if not data == b'':
+            print(data.decode())
+            break
+
+if read_or_write == 's':
+    send.close()
+elif read_or_write == 'r':
+    receive.close()
